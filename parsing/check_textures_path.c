2abb36e6bd73e68a6b47ebdaa96c2638d0e4c7e4
Diana catherine Salamanca leguizamon <dianitasale@gmail.com>
2020-09-03 09:21:04 +0200

    returning false when a file is not valid

--- parsing/check_textures_path.c
+++ parsing/check_textures_path.c
@@ -18,10 +18,12 @@ int		ft_check_north_path(char *header, char *element, char **north_path)
 	{
 		if (*north_path != NULL)
 			return (ft_put_error("argument(s) already for NO exist(s)"));
-		if (!element)
+		else if (!element)
 			return (ft_put_error("invalid arguments for NO texture"));
-		if (ft_check_path(element))
+		else if (ft_check_path(element))
 			*north_path = ft_strdup(element);
+		else 
+			return (0);
 	}
 	return (1);
 }
@@ -32,10 +34,12 @@ int		ft_check_south_path(char *header, char *element, char **south_path)
 	{
 		if (*south_path != NULL)
 			return (ft_put_error("argument(s) already for SO exist(s)"));
-		if (!element)
+		else if (!element)
 			return (ft_put_error("invalid arguments for SO texture"));
-		if (ft_check_path(element))
+		else if (ft_check_path(element))
 			*south_path = ft_strdup(element);
+		else 
+			return (0);
 	}
 	return (1);
 }
@@ -46,10 +50,12 @@ int		ft_check_west_path(char *header, char *element, char **west_path)
 	{
 		if (*west_path != NULL)
 			return (ft_put_error("argument(s) already for WE exist(s)"));
-		if (!element)
+		else if (!element)
 			return (ft_put_error("invalid arguments for WE texture"));
-		if (ft_check_path(element))
+		else if (ft_check_path(element))
 			*west_path = ft_strdup(element);
+		else
+			return (0);
 	}
 	return (1);
 }
@@ -60,10 +66,12 @@ int		ft_check_east_path(char *header, char *element, char **east_path)
 	{
 		if (*east_path != NULL)
 			return (ft_put_error("argument(s) already for EA exist(s)"));
-		if (!element)
+		else if (!element)
 			return (ft_put_error("invalid arguments for EA texture"));
-		if (ft_check_path(element))
+		else if (ft_check_path(element))
 			*east_path = ft_strdup(element);
+		else
+			return (0);
 	}
 	return (1);
 }
@@ -74,10 +82,12 @@ int		ft_check_sprite_path(char *header, char *element, char **sprite_path)
 	{
 		if (*sprite_path != NULL)
 			return (ft_put_error("argument(s) already for sprite exist(s)"));
-		if (!element)
+		else if (!element)
 			return (ft_put_error("invalid arguments for sprite texture"));
-		if (ft_check_path(element))
+		else if (ft_check_path(element))
 			*sprite_path = ft_strdup(element);
+		else 
+			return (0);
 	}
 	return (1);
 }
